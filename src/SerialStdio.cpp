0d5ce3f67aa5e33e92f2625d4662fc9217deca81
Мдфв <v.v.titerez@gmail.com>
2026-02-13 08:56:14 +0200

    Changed input to not depend on Serial

--- src/SerialStdio.cpp
+++ src/SerialStdio.cpp
@@ -6,6 +6,13 @@ static int serial_putchar(char c, FILE* f) {
     return 0;
 }
 
+static int serial_getchar(FILE* f) {
+    while (!Serial.available()) {
+        // Wait for data
+    }
+    return Serial.read();
+}
+
 static FILE serial_stdout;
 //serial_stdout is a global FILE structure that represents the serial output stream
 
@@ -13,6 +20,7 @@ void initSerialStdio() {
     Serial.begin(9600);
     //1 baud = 1 bit per second
 
-    fdev_setup_stream(&serial_stdout, serial_putchar, NULL, _FDEV_SETUP_WRITE);
+    fdev_setup_stream(&serial_stdout, serial_putchar, serial_getchar, _FDEV_SETUP_RW);
     stdout = &serial_stdout;
+    stdin = &serial_stdout;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -16,24 +16,27 @@ void setup() {
 }
 
 void loop() {
-    if (Serial.available()) {
-        int len = Serial.readBytesUntil('\n', command, sizeof(command) - 1);
-        command[len] = '\0';
+    fgets(command, sizeof(command), stdin);
+    // Remove both \r and \n
+    command[strcspn(command, "\r\n")] = '\0';
+    
+    for (int i = 0; command[i]; i++) {
+        command[i] = tolower(command[i]);
+    }
 
-        if (strcmp(command, "led on") == 0) {
-            led.on();
-            printf("LED turned ON\n");
-        }
-        else if (strcmp(command, "led off") == 0) {
-            led.off();
-            printf("LED turned OFF\n");
-        } 
-        else if (strcmp(command, "led alert") == 0) {
-            led.blink_alert();
-            printf("LED blinked for alert\n");
-        }
-        else {
-            printf("Unknown command: %s\n", command);
-        }
+    if (strcmp(command, "led on") == 0) {
+        led.on();
+        printf("LED turned ON\n");
+    }
+    else if (strcmp(command, "led off") == 0) {
+        led.off();
+        printf("LED turned OFF\n");
+    } 
+    else if (strcmp(command, "led alert") == 0) {
+        led.blink_alert();
+        printf("LED blinked for alert\n");
+    }
+    else {
+        printf("Unknown command: %s\n", command);
     }
 }
